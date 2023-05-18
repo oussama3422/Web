b28d1e33d73fe9c36fef68c9be6f0d0681eeb682
oussama3422 <ossamarhayrhay2001@gmail.com>
2023-05-18 22:54:12 +0100

    init

--- components/Prompt.jsx
+++ components/Prompt.jsx
@@ -1,5 +1,5 @@
 'use client';
-import {useState} from 'react';
+import {useState} from 'react'; 
 import Image from 'next/image';
 import { useSession } from 'next-auth/react';
 import {usePathname,useRouter} from 'next/navigation';
