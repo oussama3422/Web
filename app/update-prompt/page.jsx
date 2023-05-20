ee5d0e6d9bdf27dd432c98928597cb9ec85d9f8d
oussama3422 <ossamarhayrhay2001@gmail.com>
2023-05-20 15:12:31 +0100

    change

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -15,22 +15,25 @@ import  Form  from '@components/Form';
     useEffect(()=>{
        const getPromptDetails = async()=>{
             const response = await fetch(`/api/prompt/${promptId}`)
-            const data=response.json();
+            const data=await response.json();
             setPost({
                 prompt:data.prompt,
                 tag:data.tag,
             })
         }
+        if(promptId) getPromptDetails();
     },[promptId])
-    const createPrompt = async (e)=>{
+    const updatePrompt = async (e)=>{
         e.preventDefault();
         setSubmitting(true);
+
+        if(!promptId) return alert("Prompt ID not found");
+
         try{
-            const response = await fetch("/api/prompt/new",{
-                method: 'POST',
+            const response = await fetch(`/api/prompt/${promptId} `,{
+                method: 'PATCH',
                 body: JSON.stringify({
                     prompt: post.prompt,
-                    userId: session?.user.id,
                     tag: post.tag
                 })
             });
@@ -53,11 +56,11 @@ import  Form  from '@components/Form';
     }
   return (
     <Form
-       type="Create"
+       type="Edit"
        post={post}
        setPost={setPost}
        submiting={submitting}
-       handleSubmit={createPrompt}
+       handleSubmit={updatePrompt}
     />
   )
 }
